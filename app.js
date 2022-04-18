0930f8c6a38cc352f4110eb5d8ad56b3c9e86450
Anthony Chablov <achablov@myseneca.ca>
2022-04-18 00:22:03 -0400

    added js and changed css/html

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+// Classes of the components of form
+// The Items
+class Item{
+    constructor(item,brand, price){
+        this.item= item;
+        this.brand = brand;
+        this.price=price;
+    }
 
+}
+// The UI
+class UI{
+
+}
+
+// Local Storage
+class Storage{
+
+}
+
+
+
+// Event listeners
+// Selecting components
+//
+let buttonSubmit = document.querySelector('#button')
+buttonSubmit.addEventListener(('click'), ((e) =>
+{
+    let itemName = document.querySelector('#Item-Name');
+    let brandName = document.querySelector('Brand-Name');
+    let price = document.querySelector('Price');
+
+        
+    console.log(`button clicked`);
+    e.preventDefault()
+
+}))
